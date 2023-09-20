aa08f369d4bc27f78bf00eb92b3483246df0443c
Jair Francisco <jairfrancisco2018@gmail.com>
2023-09-19 21:11:27 -0300

    Formik application

--- src/initial.js
+++ src/initial.js
@@ -1,35 +1,34 @@
-import React from "react"
-import { createStackNavigator } from "@react-navigation/stack"
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import EntryScreen from "./screens/entry"
-import SignIn from "./screens/signIn"
-import PaymentScreen from "./screens/payment"
-import NavigationBar from "./navigation"
+import EntryScreen from "./screens/entry";
+import SignIn from "./screens/signIn";
+import PaymentScreen from "./screens/payment";
+import NavigationBar from "./navigation";
 
 const Initial = () => {
+  const Stack = createStackNavigator();
 
-    const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName="SignIn"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="signIn" component={SignIn} />
+      {/* <Stack.Screen name="signUp" component={SignUp} /> */}
 
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            
-            <Stack.Screen name="signIn" component={SignIn} />
-            {/* <Stack.Screen name="signUp" component={SignUp} /> */}
-
-            {/* ActionsTab */}
-            <Stack.Screen name="Entry" component={EntryScreen} />
-            {/* <Stack.Screen name="Shop" component={} />
+      {/* ActionsTab */}
+      <Stack.Screen name="Entry" component={EntryScreen} />
+      {/* <Stack.Screen name="Shop" component={} />
             <Stack.Screen name="Wallet" component={} /> */}
-            <Stack.Screen name="Payment" component={PaymentScreen} />
+      <Stack.Screen name="Payment" component={PaymentScreen} />
 
-            {/* BottomBar */}
-            <Stack.Screen name="BottomBar" component={NavigationBar} />
-        </Stack.Navigator>
-    )
-}
+      {/* BottomBar */}
+      <Stack.Screen name="BottomBar" component={NavigationBar} />
+    </Stack.Navigator>
+  );
+};
 
 export default Initial;
--- src/screens/signIn.js
+++ src/screens/signIn.js
@@ -1,54 +1,46 @@
 import React, { useState } from "react";
-import {
-    View, Text, TextInput, Button
-} from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import Auth from "../contexts/auth";
+
+import { Formik } from "formik";
 
 import { useNavigation } from "@react-navigation/native";
 
 const SignIn = () => {
+  const { log } = Auth();
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const navigation = useNavigation();
 
-    const { log }  = Auth();
-    const [nome, setNome] = useState('');
-    const [email, setEmail] = useState('');
-    const navigation = useNavigation();
+  const handleLogin = () => {
+    log(nome, email);
+    navigation.navigate("BottomBar");
+  };
 
-    const handleLogin = () => {
-        log(nome, email)
-        navigation.navigate("BottomBar")
-    }
-
-    return (
-
-        <View style={{backgroundColor: "#DADADA", justifyContent: "center", alignItems: "center", marginTop: "50%"}}>
-            <Text>
-                Nome:
-            </Text>
-            <TextInput 
-                name='nome'
-                value={nome}
-                onChangeText={setNome}
-                placeholder="Digite seu nome..."
-            />
-            <Text>
-                E-mail:
-            </Text>
+  return (
+    <View>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
             <TextInput
-                name='email'    
-                value={email}
-                onChangeText={setEmail}
-                placeholder="Digite seu e-mail..."
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
             />
-
-            <Button
-                title="Acessar"
-                onPress={handleLogin}
-            >
-
-            </Button>
-
-        </View>
-    )
-}
+            <TextInput
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
 
 export default SignIn;
