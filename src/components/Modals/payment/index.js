e87ab56736eade89c056477f9579a0a8a84c4a10
Jair Francisco <jairfrancisco2018@gmail.com>
2023-09-19 23:51:35 -0300

    Reestruturação Modal

--- src/components/Modals/payment/index.js
+++ src/components/Modals/payment/index.js
@@ -1,115 +1,108 @@
 import React, { useState, useRef } from "react";
-import {
-    Content,
-    Title,
-    Press,
-    Footer,
-} from "./styles"
+import { Content, Title, Press, Footer, PressPay } from "./styles";
 
-import {
-    Keyboard
-} from "react-native"
+import { Text } from "react-native";
 
-import { Picker } from "@react-native-picker/picker"
+import { Picker } from "@react-native-picker/picker";
 
 import { Modalize } from "react-native-modalize";
 
 import { Feather } from "@expo/vector-icons";
 
-
 const PayModal = ({ modalizeRef }) => {
-    const [isSelected, setSelection] = useState(false)
-    const [selectLang, setLang] = useState("")
-    const pickerRef = useRef();
+  const [isSelected, setSelection] = useState(false);
+  const [selectLang, setSelectLang] = useState("");
+  const pickerRef = useRef();
 
-    function open() {
-        pickerRef.current.focus();
-    }
-    function close() {
-        pickerRef.current.close();
-    }
+  function open() {
+    pickerRef.current.focus();
+  }
+  function close() {
+    pickerRef.current.close();
+  }
 
-    return (
-        <Modalize
-            modalStyle={{
-                flex: 1,
-                justifyContent: "center",
-                backgroundColor: "#DADADA",
-                borderTopEndRadius: 20,
-                borderTopStartRadius: 20,
-            }}
-            ref={modalizeRef}
-            modalHeight={500}
-            panGestureEnabled={false}
-            openAnimationConfig={{
-                timing: { duration: 400 },
-                spring: { speed: 20, bounciness: 10 }
-            }}
-            closeAnimationConfig={{
-                timing: { duration: 400 },
-                spring: { speed: 20, bounciness: 10 }
-            }}
+  return (
+    <Modalize
+      modalStyle={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#DADADA",
+        borderTopEndRadius: 20,
+        borderTopStartRadius: 20,
+      }}
+      ref={modalizeRef}
+      modalHeight={650}
+      panGestureEnabled={false}
+      openAnimationConfig={{
+        timing: { duration: 400 },
+        spring: { speed: 20, bounciness: 10 },
+      }}
+      closeAnimationConfig={{
+        timing: { duration: 400 },
+        spring: { speed: 20, bounciness: 10 },
+      }}
+      HeaderComponent={
+        <Footer>
+          <Press title="" onPress={() => modalizeRef.current.close()}>
+            <Feather name="arrow-down" size={30} color={"#000"} />
+          </Press>
+        </Footer>
+      }
+    >
+      <Content>
+        <Title>Selecione o método de pagamento:</Title>
 
-            HeaderComponent={
+        <Picker
+          style={{
+            alignSelf: "center",
 
-                <Footer>
-                    <Press
-                        title=""
-                        onPress={() => modalizeRef.current.close()}
-                    >
-                        <Feather name="arrow-down" size={30} color={"#fff"} />
-                    </Press>
-                </Footer>
-            }
+            fontSize: 24,
+            backgroundColor: "#009688",
+            borderRadius: 10,
+            paddingVertical: 10,
+            paddingHorizontal: 12,
+
+            width: "80%",
+            borderRadius: 10,
+          }}
+          ref={pickerRef}
+          selectedValue={selectLang}
+          onValueChange={(itemValue, itemIndex) => {
+            setSelectLang(itemValue);
+          }}
+          itemStyle={{
+            borderRadius: 40,
+          }}
         >
-            <Content>
-                <Title>
-                    Selecione o método de pagamento:
-                </Title>
+          <Picker.Item
+            label="PIX"
+            value={"pix"}
+            style={{
+              fontSize: 24,
+            }}
+          />
+          <Picker.Item
+            label="CARTÃO"
+            value={"cartao"}
+            style={{
+              fontSize: 24,
+            }}
+          />
+          <Picker.Item
+            label="BOLETO"
+            value={"boleto"}
+            style={{
+              fontSize: 24,
+            }}
+          />
+        </Picker>
 
-                <Picker
-
-                    style={{
-                        alignSelf: "center",
-
-                        fontSize: 24,
-                        backgroundColor: "#009688",
-                        borderRadius: 10,
-                        paddingVertical: 10,
-                        paddingHorizontal: 12,
-
-                        width: "80%",
-                        borderRadius: 10,
-                    }}
-                    ref={pickerRef}
-                    selectValue={selectLang}
-                    onValueChange={(itemValue) => setLang(itemValue)}
-                    itemStyle={{
-                        borderRadius: 40,
-                    }}
-                >
-                    <Picker.Item 
-                        label="Java"
-                        value={"Java"}
-                        style={{
-                            fontSize: 24,
-                        }}  
-                    />
-                    <Picker.Item
-                        label="Javascript"
-                        value={"Javascript"}
-                    />
-
-
-                </Picker>
-
-
-                
-
-            </Content>
-        </Modalize>
-        
-    )
-}
+        <PressPay>
+          <Text>PAGAR</Text>
+        </PressPay>
+      </Content>
+    </Modalize>
+  );
+};
 
 export default PayModal;
--- src/components/Modals/payment/styles.js
+++ src/components/Modals/payment/styles.js
@@ -1,31 +1,37 @@
 import styled from "styled-components/native";
 
 export const Content = styled.View`
-    width: 90%;
-    justify-self: center;
-    align-self: center;
-    
-    margin-top: 50px;
-    background-color: #DADADA;
+  width: 90%;
+  height: 90%;
+  justify-self: center;
+  align-self: center;
+
+  margin-top: 10px;
+  background-color: #dadada;
 `;
 
 export const Title = styled.Text`
-    font-size: 28;
-    font-weight: bold;
-    margin: 30px;
-
+  font-size: 28;
+  font-weight: bold;
+  margin: 30px;
 `;
 
 export const Footer = styled.View`
-    width: 100%;
-    align-self: center;
-
+  width: 100%;
+  align-self: center;
 `;
 
 export const Press = styled.TouchableOpacity`
-    width: fit-content;
-    height: 30px;
-    background-color: #DADADA;
+  align-items: center;
+`;
 
-    align-items: center;
+export const PressPay = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  margin-top: 40%;
+  width: 60%;
+  height: 40px;
+
+  background-color: #dadaff;
 `;
